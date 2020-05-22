cd40ed17ab802f61e2bc73ed6b7a2f6a54416861
xueyuheng <1428591636@qq.com>
2020-05-22 10:13:20 +0800

    增加05 md

--- 05/src/datatype.ts
+++ 05/src/datatype.ts
@@ -1,6 +1,6 @@
 //原始类型
 let bool: boolean = true;
-let num: number = 123
+let num: number | undefined | null = 123;      //联合类型
 let str: string = 'abc'
 
 //数组类型
@@ -32,9 +32,27 @@ let s1: symbol = Symbol()
 let s2 = Symbol()
 console.log(s1 === s2) //fasle
 
+//undefined  null
+let un: undefined = undefined;
+let nu: null = null;
 
+//在ts中 undefined 和 null 为所有类型的子类型  需要在tsconfig.json中找到 "strictNullChecks" 设置为 false,   --解决方式一
+//如果想使用严格的语法  可以使用联合类型
+num = undefined
+num = null
 
+// void   是一种操作符 可以让任何东西返回 undefined  表示没有任何返回值的函数
+let noReturn = () => {}
 
+// any
+let x;
 
+// never
+let error = () => {
+    throw new Error('error')
+}
+let endless = () => {
+    while(true) {}
+}
 
 
